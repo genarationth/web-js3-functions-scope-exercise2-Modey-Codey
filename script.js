cac6872996da21c11f370d1d580ad1e4af598022
Modey Codey <phawngam@hotmail.com>
2024-05-14 15:30:24 +0700

    Ex 2.1 2.2

---
+++ script.js
@@ -0,0 +1,31 @@
+// Ex2.1
+const evenOrOdd = function(a) { 
+    if (a % 2 === 0) {
+            console.log("even number");
+        } else {
+            console.log("odd number");
+        }
+};
+evenOrOdd(9);
+
+// Ex2.2
+const evenOrOdd2 = (a) => { 
+    if (a % 2 === 0) {
+        console.log("even number");
+    } else {
+        console.log("odd number");
+    }  
+};
+evenOrOdd2(8);
+
+// Ex2.1
+const evenNumber = function(a) { 
+    if (a % 2 === 0) {
+            console.log("even number");
+    }
+};
+evenNumber(8);
+
+//Ex2.2
+const evenNumber2 = a => {if(a % 2 === 0) console.log("even number")};
+evenNumber2(4);
